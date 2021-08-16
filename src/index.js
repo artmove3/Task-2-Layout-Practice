cedfb27a92a0f5be6723a62a8047b9c9e2343061
artmove3 <artmove3@gmail.com>
2021-08-16 21:59:31 +0700

    added russian region to datepicker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import './styles/styles.scss'
 // import * as $ from 'jquery'
 require('webpack-jquery-ui')
 
-$('.card__date input').datepicker({
-    inline: true,
-    showOtherMonths: true,
-    dayNamesMin: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
-})
+$.datepicker.regional["ru"] = {
+    closeText: "Закрыть",
+    prevText: "&#x3C;Пред",
+    nextText: "След&#x3E;",
+    currentText: "Сегодня",
+    monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
+    monthNamesShort: [ "Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+    "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" ],
+    dayNames: [ "воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота" ],
+    dayNamesShort: [ "вск", "пнд", "втр", "срд", "чтв", "птн", "сбт" ],
+    dayNamesMin: [ "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
+    weekHeader: "Нед",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "" };
+$.datepicker.setDefaults( $.datepicker.regional["ru"] )
+$(function(){
+    $('.datepicker').datepicker()
+    
+    
+  })
