17ab5fee4acd8818f86d3caf598871747ff4779b
artmove3 <artmove3@gmail.com>
2022-04-26 15:53:07 +0700

    complete counter logic, still have issues with css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/styles.scss'
 require('webpack-jquery-ui')
 
+
 $.datepicker.regional["ru"] = {
     closeText: "Закрыть",
     prevText: "&#x3C;Пред",
@@ -25,16 +26,74 @@ $.datepicker.regional["ru"] = {
 $.datepicker.setDefaults( $.datepicker.regional["ru"] );
 
 $(function(){
-    let firstDay
-    let lastDay
     const guestCounterBlock = document.querySelector('.card__guests .input__container')
     const guestCounter = document.querySelector('.guest__counter')
+    const guestCounterCells = document.querySelectorAll('.counter__cell')
+    let currentCount = guestCounterBlock.childNodes[0].value
+    const inputContent = document.querySelector('.card__guests .input__container input')
+    const applyButton = document.querySelector('.apply__buttons .button__apply')
+    const refreshButton = document.querySelector('.apply__buttons .button__refresh')
 
+    function setCount(num) {
+        inputContent.innerHTML += num
+    }
+    function openCounter() {
+        guestCounter.style.display = 'block'
+    }
+
+    function closeCounter() {
+        guestCounter.style.display = 'none'
+    }
 
     guestCounterBlock.addEventListener('click', () => {
-        guestCounter.style.display = 'block'
+        guestCounterBlock.classList.add('active')
+        openCounter()
+        console.log(currentCount)
+        console.log(inputContent)
     })
-        
+
+    applyButton.addEventListener('click', () => closeCounter())
+    refreshButton.addEventListener('click', () => {
+        setCount(0)
+        currentCount = 0
+        guestCounterCells.forEach(cell => {
+            cell.childNodes[1].childNodes[1].value = 0
+        })
+        closeCounter()
+    })
+
+    
+    // $('.card__guests .input__container').on('click', () => {
+    //     $('.guest__counter').addClass('.active')
+    // })
+
+    guestCounterCells.forEach(cell => {
+        const buttonMinus = cell.querySelector('.button__minus')
+        const buttonPlus = cell.querySelector('.button__plus')
+        const currentCounter = cell.querySelector('.counter')
+
+
+
+        buttonMinus.addEventListener('click', () => {
+           setCount(--currentCount)
+            currentCounter.value--
+            if(currentCounter.value == 0) {
+                buttonMinus.setAttribute('disabled','')
+            }
+        })
+
+        buttonPlus.addEventListener('click', () => {
+            setCount(++currentCount)
+            currentCounter.value++
+            if(currentCounter.value > 0) {
+                buttonMinus.removeAttribute('disabled','')
+                refreshButton.style.visibility = 'visible'
+            }
+        }) 
+
+
+    });
+
 
     
     $('.datepicker__first').datepicker({
